751d856df98c3f9e5325d2d721dc8e09fc4d60a7
Alexey Chuprin <a.chuprin@capjack.ru>
2024-10-06 18:26:21 +0300

    Code cleanup

--- csi-core-server/src/main/kotlin/org/shypl/csi/core/server/ConnectionAuthorizer.kt
+++ csi-core-server/src/main/kotlin/org/shypl/csi/core/server/ConnectionAuthorizer.kt
@@ -1,6 +1,6 @@
 package org.shypl.csi.core.server
 
 interface ConnectionAuthorizer<I : Any> {
-	fun authorizeConnection(key: ByteArray): I?
+	fun authorizeConnection(data: ByteArray): I?
 }
 
--- csi-core-server/src/test/kotlin/org/shypl/csi/core/server/_test/TestConnectionAuthorizer.kt
+++ csi-core-server/src/test/kotlin/org/shypl/csi/core/server/_test/TestConnectionAuthorizer.kt
@@ -4,8 +4,8 @@ import org.shypl.csi.core.server.ConnectionAuthorizer
 import org.shypl.tool.io.getInt
 
 class TestConnectionAuthorizer : ConnectionAuthorizer<Int> {
-	override fun authorizeConnection(key: ByteArray): Int? {
-		val clientId = key.getInt(0)
+	override fun authorizeConnection(data: ByteArray): Int? {
+		val clientId = data.getInt(0)
 		return if (clientId == 0) null else clientId
 	}
 }
